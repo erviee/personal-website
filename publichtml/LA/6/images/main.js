30ce69cd056ccba4c61206ba09bc3a90a9ac5850
erviee <148853259+erviee@users.noreply.github.com>
2023-10-29 00:16:37 +0800

    Add files via upload

---
+++ publichtml/LA/6/images/main.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Javascript is EXCITING!!!";
+    document.getElementById("demo").style.fontSize = "30px";
+    document.getElementById("demo").style.margin = "20px";
+    document.getElementById("demo").style.padding = "20px";
+    document.getElementById("demo").style.color = "black";
+    document.getElementById("demo").style.backgroundColor = "yellow";
+    document.getElementById("demo").style.textAlign = "center";
+}
+function myFunction1() {
+    document.getElementById("demo1").innerHTML = "sure na yan?";
+    document.getElementById("demo1").style.fontSize = "30px";
+    document.getElementById("demo1").style.color = "black";
+    document.getElementById("demo1").style.margin = "20px";
+    document.getElementById("demo1").style.padding = "20px";
+    document.getElementById("demo1").style.backgroundColor = "blue"
+    document.getElementById("demo1").style.textAlign = "center";
+}
+    
